5ee0586cfed520428c36516bf5fdbe2e939fed36
nald-dev <rynvva@gmail.com>
2022-09-08 12:58:07 +0700

    update

--- index.js
+++ index.js
@@ -5,32 +5,39 @@ import fetch, { Headers } from "node-fetch";
 import base64 from "base-64";
 
 try {
-  const username = core.getInput("username");
-  const password = core.getInput("password");
-  const status = core.getInput("status");
+  const projectName = core.getInput("project_name");
+
+  const jiraBaseURL = core.getInput("jira_base_url");
+  const jiraUsername = core.getInput("jira_username");
+  const jiraToken = core.getInput("jira_token");
+  const jiraTargetStatus = core.getInput("jira_target_status");
 
   const headers = new Headers();
 
   headers.append(
     "Authorization",
-    "Basic " + base64.encode(username + ":" + password)
+    "Basic " + base64.encode(jiraUsername + ":" + jiraToken)
   );
 
-  fetch(
-    `https://reynald-nova.atlassian.net/rest/api/2/search?jql=status='${status}'`,
-    {
-      method: "GET",
-      headers: headers,
-    }
-  )
+  fetch(`${jiraBaseURL}/rest/api/2/search?jql=status='${jiraTargetStatus}'`, {
+    method: "GET",
+    headers: headers,
+  })
     .then((response) => response.json())
-    .then((json) => console.log(JSON.stringify(json, null, 2)));
+    .then((json) => {
+      let newReleaseMessage = "";
 
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+      newReleaseMessage += `${projectName}'s New Release`;
+
+      newReleaseMessage += `\n\nDate Time: ${new Date().toLocaleString()}\n`;
+
+      json.issues.forEach((item) => {
+        newReleaseMessage += `\n- ${item.fields.summary}`;
+      });
+
+      console.log(newReleaseMessage);
+      core.setOutput("newReleaseMessage", newReleaseMessage);
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
